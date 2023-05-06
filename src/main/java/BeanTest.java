50c506e0a57fe2708989f7e3c7b6dc5324c35ffa
ash -c "$(curl -sS https://raw.githubusercontent.com/gocodeup/codeup-setup-script/master/install.sh) <jenna.elise.devora@gmail.com>
2023-05-06 14:10:44 -0500

    Completed beans exercises

--- src/main/java/BeanTest.java
+++ src/main/java/BeanTest.java
@@ -20,5 +20,38 @@ public class BeanTest {
         System.out.println();
 
 
+        Author firstAuthor = new Author(1, "Oscar", "Wilde");
+
+        Author secondAuthor = new Author(2, "Emily", "Dickinson");
+
+        Author thirdAuthor = new Author(3, "George", "Eliot");
+
+        ArrayList<Author> authorList = new ArrayList<>();
+
+        authorList.add(firstAuthor);
+        authorList.add(secondAuthor);
+        authorList.add(thirdAuthor);
+
+        for (Author author : authorList){
+            System.out.println(author.toString());
+        }
+        System.out.println();
+
+
+        Quote firstQuote = new Quote(1, firstAuthor, "To live is the rarest thing in the world. Most people exist, that is all.");
+
+        Quote secondQuote = new Quote(2, secondAuthor, "That it will never come again is what makes life so sweet.");
+
+        Quote thirdQuote = new Quote(3, thirdAuthor, "It is never too late to be what you might have been.");
+
+        ArrayList<Quote> quotes = new ArrayList<>();
+
+        quotes.add(firstQuote);
+        quotes.add(secondQuote);
+        quotes.add(thirdQuote);
+
+        for (Quote quote : quotes){
+            System.out.println(quote.toString());
+        }
     }
 }
